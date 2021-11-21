d1a37ff48bab5b9e634023d195f11aed876ce8f6
Weinber Krisztián <wkrissz97@gmail.com>
2021-11-21 18:07:03 +0100

    feat. add synchronous event event sending example

--- Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -3,6 +3,7 @@ package com.learnkafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +11,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
     LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
 
         // invoke kafka producer
+        log.info("before sendLibraryEvent");
         libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
--- Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -36,6 +38,25 @@ public class LibraryEventProducer {
                 handleSuccess(key, value, result);
             }
         });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        Integer key = libraryEvent.getId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        SendResult<Integer, String> result = null;
+
+        // get will wait until the future resumes
+        try {
+            result = kafkaTemplate.sendDefault(key, value).get();
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("ExecutionException/InterruptedException Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return result;
     }
 
     private void handleFailure(Integer key, String value, Throwable ex) {
