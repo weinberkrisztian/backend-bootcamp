ff31946ad62c6bbc19e00e36010d3bff8b558472
Weinber Krisztián <wkrissz97@gmail.com>
2021-11-21 15:37:28 +0100

    feat. create LibraryEvent and Book entities

---
+++ Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/domain/Book.java
@@ -0,0 +1,16 @@
+package com.learnkafka.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Book {
+    private Integer id;
+    private String name;
+    private String author;
+}
---
+++ Kafka/codebase/library-events-producer/src/main/java/com/learnkafka/domain/LibraryEvent.java
@@ -0,0 +1,15 @@
+package com.learnkafka.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+    private Integer id;
+    private Book book;
+}
